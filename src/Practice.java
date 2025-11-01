dc0b295ace16ab275bddbfbd9c74ca11095dfac4
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-11-01 15:12:54 -0700

    Implemented the second solution for maxDiff.

--- src/Practice.java
+++ src/Practice.java
@@ -20,8 +20,23 @@ public class Practice {
          *  int[] newList.sort = varible;
          * return newlist[0] - newlist[len(newlist) - 1]
          */
-        Arrays.sort(nums);
-        return nums[nums.length - 1] - nums[0];
+        /*first method */
+        // Arrays.sort(nums);
+        // return nums[nums.length - 1] - nums[0];
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max - min;
     }
 
 
